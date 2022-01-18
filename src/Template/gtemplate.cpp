506e78f19f269091065ab6787104cdd6822462a7
John Anchery <etcetra7n@gmail.com>
2022-01-18 22:21:46 +0530

    Update gtemplate.cpp

--- src/Template/gtemplate.cpp
+++ src/Template/gtemplate.cpp
@@ -1,9 +1,62 @@
-#include <iostream>
+#include <cstdlib>
 #include <vector>
+#include <string>
+#include <map>
 #include "Template/gtemplate.h"
 
 namespace Template
 {
+    Element::Element()
+    {
+        _parent = nullptr;
+    }
+
+    std::string Element::name()
+    {
+        return _name;
+    }
+
+    std::string Element::value()
+    {
+        return _value;
+    }
+
+    std::map<std::string, std::string> Element::attributes()
+    {
+        return _attr;
+    }
+
+    std::vector<Element*> Element::children()
+    {
+        return _children;
+    }
+
+    Element *Element::parent()
+    {
+        return _parent;
+    }
+
+    void Element::set_name(const std::string name)
+    {
+        _name = name;
+    }
+
+    void Element::set_value(const std::string value)
+    {
+        _value = value;
+    }
+
+    void Element::add_attr(const std::string name, const std::string value)
+    {
+        _attr.insert({name, value});
+    }
+
+    void Element::append_child(Element* child)
+    {
+        _children.push_back(child);
+        child->_parent = this;
+    }
+
     Element *Gtemplate::new_element()
     {
         Element *new_elem = new Element();
