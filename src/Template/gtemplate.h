d7158100ff5bf28e382676c31860d4f47522861e
John Anchery <etcetra7n@gmail.com>
2022-01-18 22:26:51 +0530

    Update gtemplate.h

--- src/Template/gtemplate.h
+++ src/Template/gtemplate.h
@@ -4,33 +4,10 @@
 #include <vector>
 #include <string>
 #include <map>
+#include "Template/element.h"
 
 namespace Template
 {
-    class Element;
-    class Gtemplate;
-
-    class Element
-    {
-      private:
-        std::string _name;
-        std::string _value;
-        std::map<std::string, std::string> _attr;
-        Element *_parent;
-        std::vector<Element*> _children;
-      public:
-        Element();
-        std::string name();
-        std::string value();
-        std::map<std::string, std::string> attributes();
-        std::vector<Element*> children();
-        Element *parent();
-        void set_name(const std::string);
-        void set_value(const std::string);
-        void add_attr(const std::string, const std::string);
-        void append_child(Element*);
-    };
-
     class Gtemplate
     {
       private:
