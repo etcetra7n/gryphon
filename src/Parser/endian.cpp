fd54abcd72b30000c4d59748c16e039382b6ba0c
John Anchery <etcetra7n@gmail.com>
2022-01-20 13:07:57 +0530

    Rename macros to avoid collisions
    - `BIG_ENDIAN` is now `BIG_ENDIAN_ORDER`
    - `LITTLE_ENDIAN` is now `LITTLE_ENDIAN_ORDER`

--- src/Parser/endian.cpp
+++ src/Parser/endian.cpp
@@ -6,6 +6,6 @@ namespace Parser
     uint8_t endian()
     {
         uint16_t _tnum = 0x0001;
-        return (reinterpret_cast<char*>(&_tnum))[1]? BIG_ENDIAN : LITTLE_ENDIAN;
+        return (reinterpret_cast<char*>(&_tnum))[1]? BIG_ENDIAN_ORDER : LITTLE_ENDIAN_ORDER;
     }
 }
--- src/Parser/endian.h
+++ src/Parser/endian.h
@@ -3,19 +3,19 @@
 
 #include <stdint.h>
 
-#define BIG_ENDIAN 1
-#define LITTLE_ENDIAN 2
+#define BIG_ENDIAN_ORDER 1
+#define LITTLE_ENDIAN_ORDER 2
 
 namespace Parser
 {
     uint8_t endian();
     inline uint32_t hton_32(const uint32_t x)
     {
-        return ((endian() == BIG_ENDIAN)? x : __builtin_bswap32(x));
+        return ((endian() == BIG_ENDIAN_ORDER)? x : __builtin_bswap32(x));
     }
     inline uint64_t hton_64(const uint64_t x)
     {
-        return ((endian() == BIG_ENDIAN)? x : __builtin_bswap64(x));
+        return ((endian() == BIG_ENDIAN_ORDER)? x : __builtin_bswap64(x));
     }
 }
 
