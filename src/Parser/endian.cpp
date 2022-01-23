1e7e6e8e1fc433d461691271ee037285bfa77566
John Anchery <etcetra7n@gmail.com>
2022-01-23 14:07:27 +0530

    Update endian.cpp

--- src/Parser/endian.cpp
+++ src/Parser/endian.cpp
@@ -3,7 +3,7 @@
 
 namespace Parser
 {
-    uint8_t endian()
+    constexpr uint8_t endian()
     {
         uint16_t _tnum = 0x0001;
         return (reinterpret_cast<char*>(&_tnum))[1]? BIG_ENDIAN_ORDER : LITTLE_ENDIAN_ORDER;
