cb98c34a7957899d6a2fd9bc569012d5b8a703d6
John Anchery <etcetra7n@gmail.com>
2022-02-01 13:34:18 +0530

    Decrease tablulation space to 2

--- src/Template/dom.h
+++ src/Template/dom.h
@@ -12,34 +12,34 @@ namespace Template
     class Element
     {
       private:
-        std::string                           _name;
-        std::string                           _value;
-        std::map<std::string, std::string>    _attr;
-        Element*                              _parent;
-        std::vector<Element*>                 _children;
+        std::string                         _name;
+        std::string                         _value;
+        std::map<std::string, std::string>  _attr;
+        Element*                            _parent;
+        std::vector<Element*>               _children;
       public:
-                                              Element();
-        std::string                           name() const;
-        std::string                           value() const;
-        std::map<std::string, std::string>    attributes() const;
-        std::vector<Element*>                 children() const;
-        Element*                              parent() const;
-        void                                  setName(const std::string&);
-        void                                  setValue(const std::string&);
-        void                                  setAttribute(const std::string&, const std::string&);
-        void                                  setParent(Element*);
-        void                                  appendChild(Element*);
+                                            Element();
+        std::string                         name() const;
+        std::string                         value() const;
+        std::map<std::string, std::string>  attributes() const;
+        std::vector<Element*>               children() const;
+        Element*                            parent() const;
+        void                                setName(const std::string&);
+        void                                setValue(const std::string&);
+        void                                setAttribute(const std::string&, const std::string&);
+        void                                setParent(Element*);
+        void                                appendChild(Element*);
     };
 
     class Dom
     {
       private:
-        std::vector<Element*>                 _elements;
+        std::vector<Element*>               _elements;
       public:
-        void                                  reserveSpace(std::vector<Element*>::size_type);
-        Element*                              getElementByIndex(uint64_t);
-        Element*                              createElement();
-        std::vector<Element*>                 elements() const;
+        void                                reserveSpace(std::vector<Element*>::size_type);
+        Element*                            getElementByIndex(uint64_t);
+        Element*                            createElement();
+        std::vector<Element*>               elements() const;
     };
 }
 
