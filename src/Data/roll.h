c3cc2ed2887f6beee453aa28fba66809fe4458fb
John Anchery <etcetra7n@gmail.com>
2022-02-01 13:30:17 +0530

    Implement Roll class
    Not complete though

--- src/Data/roll.h
+++ src/Data/roll.h
@@ -4,18 +4,28 @@
 #include <stdint.h>
 #include <vector>
 
+#define INT32_AT   0x0000000000000000
+#define UINT32_AT  0x0000000000000001
+#define INT64_AT   0x0000000000000002
+#define UINT64_AT  0x0000000000000003
+#define FLOAT32_AT 0x0000000000000004
+#define FLOAT64_AT 0x0000000000000005
+
 namespace Data
 {
     class Roll
     {
       private:
-        uint32_t                            _type;
-        std::vector<uint32_t>               _uint32;
+        uint64_t                          _dimension;
+        uint64_t                          _size;
+        std::vector<uint32_t>             _rtype;
+        std::vector<std::vector<void*>*>  _axlist;
       public:
-                                            Roll();
-        uint32_t                            type() const;
-        std::vector<uint32_t>::size_type    size() const;
-        std::vector<uint32_t>               data() const;
+                                          Roll(uint64_t, uint64_t, const std::vector<uint32_t>&);
+        uint64_t                          size() const;                        
+        uint64_t                          dimension() const;
+        std::vector<uint32_t>             rtype() const;
+        void                              setAxis(uint64_t, std::vector<void*>&);
     };
 }
 
