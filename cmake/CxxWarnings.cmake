74e2dd28d9148d10beb9dda3bdcff881ba350d63
John Anchery <etcetra7n@gmail.com>
2022-02-02 21:08:08 +0530

    remove -Wnarrowing and better comments
    -Wall turns -Wnarrowing on by default

--- cmake/CxxWarnings.cmake
+++ cmake/CxxWarnings.cmake
@@ -1,27 +1,26 @@
 set(GCC_WARNINGS
-    -Wall
-    -Wextra
-    -Wpedantic
-    -Wcast-align       # warn for potential performance problem casts
+    -Wall              # standard and reasonable baseline warnings
+    -Wextra            # standard and reasonable baseline warnings
+    -Wpedantic         # warn on anything not adhering to strict ISO C++ standards
+    -Wcast-align       # warn on cast of pointers to types of larger alignment (eg. char* to int*)
     -Wunused           # warn on anything being unused
-    -Wconversion       # warn on type conversion that may lose data
+    -Wconversion       # warn on type conversion that may result in loss of data
     -Wnull-dereference # warn if a null dereference is detected
     -Wformat=2         # warn on security issues of functions that format output (like printf)
-    -Wnarrowing        # warn on illegal conversions like 64 bit data type to 32 bit data types or similar
 )
 set(CLANG_WARNINGS
-    -Wall
-    -Wextra
-    -Wpedantic
+    -Wall              # standard and reasonable baseline warnings
+    -Wextra            # standard and reasonable baseline warnings
+    -Wpedantic         # warn on everything not adhering to strict ISO C++ standards
     -Wcast-align       # warn for potential performance problem casts
     -Wunused           # warn on anything being unused
     -Wconversion       # warn on type conversion that may lose data
     -Wnull-dereference # warn if a null dereference is detected
-    -Wformat=2         # warn on security issues of functions that format output(ie printf)
-    -Wshorten-64-to-32 # warn on illegal conversion from 64 bit data type to 32 bit data types
+    -Wformat=2         # warn on security issues of functions that format output(like printf)
+    -Wshorten-64-to-32 # warn on illegal conversion from 64 bit data types to 32 bit data types
 )
 set(MSVC_WARNINGS
-    /W4     # Baseline reasonable warnings
+    /W4     # standard and reasonable baseline warnings
     /w14242 # 'identifier': conversion from 'type1' to 'type2', possible loss of data
     /w14311 # 'variable' pointer truncation from 'type1' to 'type2'
     /w14826 # Conversion from 'type1' to 'type2' is sign-extented. This may cause
