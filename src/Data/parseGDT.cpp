aff5779defc9ff0c592df8ada21404fddcd9e06c
John Anchery <etcetra7n@gmail.com>
2022-02-09 12:26:52 +0530

    implement parseGDT partially

--- src/Data/parseGDT.cpp
+++ src/Data/parseGDT.cpp
@@ -1,11 +1,44 @@
 #include "Data/data.h"
 
+#include <stdint.h>
+
 #include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+#include "Parser/parser.h"
+#include "Parser/endian.h"
 
 namespace Data
 {
     int parseGDT(Roll *roll, const std::string &filepath)
     {
+        std::ifstream file(filepath, std::ios::in);
+        std::stringstream buffer;
+        if(!file.good())
+        {
+            file.close();
+            return 1;
+        }
+        buffer << file.rdbuf();
+        file.close();
+
+        Parser::Parser parser(buffer.str());
+
+        if(parser.active())
+        {
+            parser.ignoreTill("gdt");
+            parser.parseInt32();
+            parser.parseInt32();
+            parser.parseInt32();
+
+            //CONTINUE
+        }
+        else
+        {
+            return 1;
+        }
         return 0;
     }
 }
