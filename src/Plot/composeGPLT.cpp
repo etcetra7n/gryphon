5fab143bedc28d7ad38ccfdc23074eda295c3903
John Anchery <etcetra7n@gmail.com>
2023-05-04 22:05:03 +0530

    Add documentation of composeGPLT function

--- src/Plot/composeGPLT.cpp
+++ src/Plot/composeGPLT.cpp
@@ -7,6 +7,15 @@ namespace Plot
 {
     int composeGPLT(Template::Dom *document, Data::Roll *roll)
     {
+        /* Basically compine the data file, which is a collection of points
+           and the template file, which dictates how to render the graph.
+           The final product, the .gplt file, can be opened on demand, to
+           render the graph (in a matplotlib-like interactive window)
+
+           The graph can as well be exported into image-like file formats
+           like .jpeg, .png, .bmp etc. However these formats are not
+           interactive
+        */
         return 0;
     }
 }
