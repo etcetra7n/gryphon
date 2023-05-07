b58ee9add0e317ef30f748edf4db2eecab6110c5
John Anchery <etcetra7n@gmail.com>
2023-05-07 12:29:54 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+import sphinx_rtd_theme
 
 source_suffix = '.rst'
 master_doc = 'index'
@@ -30,7 +31,9 @@ author = 'John Anchery'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+    "sphinx_rtd_theme"
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
